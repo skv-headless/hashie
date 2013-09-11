6435997c08c7ba5b36b1efdfeedbff1810e3d2ce
JanaVPetrova <janavpetrova@gmail.com>
2013-09-11 09:57:55 +0200

    add submash and setter

--- lib/hashie/mash.rb
+++ lib/hashie/mash.rb
@@ -5,9 +5,15 @@ module Hashie
     end
 
     def method_missing(method, *args, &block)
-      #raise method.inspect
-      if /.*\?$/.match(method)
-        false
+      if method.to_s.chars.last  == "?"
+        method = method.to_s.gsub("?", "").to_sym
+        @hash.has_key? method
+      elsif method.to_s.chars.last  == "="
+        method = method.to_s.gsub("=", "").to_sym
+        @hash[method] = args.first
+      elsif method.to_s.chars.last  == "!"
+        method = method.to_s.gsub("!", "").to_sym
+        @hash[method] = Mash.new
       else
         @hash[method]
       end
--- test/lib/mash_test.rb
+++ test/lib/mash_test.rb
@@ -12,4 +12,19 @@ class MashTest < MiniTest::Test
   def test_mash_name_return_name
     assert_nil @mash.name
   end
+
+  def test_mash_set_name
+    @mash.name = "John"
+    assert_equal "John", @mash.name
+  end
+
+  def test_mash_key_exist
+    @mash.name = "John"
+    assert @mash.name?
+  end
+
+  def test_bang_do_submash
+    @mash.author!.name = "John"
+    assert_equal "John", @mash.author.name
+  end
 end
