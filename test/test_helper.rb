68ea1c86c364a1bb2938663e5dbb6c62440b501e
JanaVPetrova <janavpetrova@gmail.com>
2013-09-12 11:46:03 +0200

    edit test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,9 @@
 require 'bundler/setup'
 Bundler.require
 
-require "minitest/autorun"
+require 'coveralls'
+Coveralls.wear!
 
-if ENV['TRAVIS']
-  require 'coveralls'
-  Coveralls.wear!
-end
+require "minitest/autorun"
 
 Dir[File.expand_path('../fixtures/*.rb', __FILE__)].each { |f| require f }
